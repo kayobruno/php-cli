d4cf0709701cd284b096dedf518d9e0772b87b25
Kayo Bruno <kayodw@gmail.com>
2021-10-03 18:42:47 -0300

    Adicionar classe UserDTO
    Adicionar classe seguindo o padrão de projeto DTO, para ajudar na
    transfência de dados entre "subsistemas".

---
+++ src/App/Models/UserDTO.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ASPTest\App\Models;
+
+final class UserDTO
+{
+    public function __construct(
+        public string $name,
+        public string $lastName,
+        public string $email,
+        public mixed $age
+    )
+    {}
+}
